97ec43212b2d87af1ee66a518c9c376ba4eac2a4
taewonnn <thisis_tw94@naver.com>
2024-01-04 11:38:19 +0900

    120803

--- programmers/120803.py
+++ programmers/120803.py
@@ -1,7 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120803?language=python3
 
 # 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
-
 # 제한사항
 # -50000 ≤ num1 ≤ 50000
 # -50000 ≤ num2 ≤ 50000
