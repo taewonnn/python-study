c5f98fc83353e826a7d65c7724eed27229fec1c1
eden <eden@rainbow.co.kr>
2025-01-09 14:13:29 +0900

    string

---
+++ basic/string.py
@@ -0,0 +1,31 @@
+print('text')
+
+print('-----')
+
+# 줄바꿈 '''
+print('''
+Hello
+world
+''') 
+# Hello
+# world
+
+print("""
+Hello
+!!!
+""") 
+# Hello
+# !!!
+print('-----')
+
+
+print('string:','1' + '1') #11
+print('hello world' * 20)
+
+
+# len
+print(len('hello world' * 20)) #220
+
+# replace
+print('hello world'.replace('world', 'univers')) # hello univers
+
