c5f07aa9407bb3087c9c465307d7e73855c78eac
eden <eden@rainbow.co.kr>
2025-01-09 14:12:07 +0900

    number

---
+++ basic/number.py
@@ -0,0 +1,21 @@
+print('hello world')
+
+print('-----')
+print(1) # int
+print(1.1) #float 
+
+print('1+1', 1+1)   # 1+1 2
+print(type(1.1)) # <class 'float'>
+
+print('-----')
+
+# math
+import math
+print(math.sqrt(4)) # 2
+print(math.pow(4, 2)) # 16
+
+# random
+import random
+print(random.random()) # 0.7938240725699279 radom
+
+
