7fc3c03430a09c453e3ef7669b062189665b0911
eden <eden@rainbow.co.kr>
2025-01-09 15:29:29 +0900

    variable

---
+++ basic/variable.py
@@ -0,0 +1,27 @@
+name = 'sori'
+
+print('hi, '+name+' ... bye '+name+',' )
+
+
+# debugging
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+print('??',a)
+g = 7
+a = 2
+h = 9
+i = 8
+print('??',a)
+j = 7
+k = 6
+l = 5
+m = 4
+n = 3
+o = 2
+p = 1
+
+print(a) #2
