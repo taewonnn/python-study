b00630ac1bd58de5e6a3337e4b4f1911566d9edd
eden <eden@rainbow.co.kr>
2025-01-10 14:01:39 +0900

    csv

--- basic/excelFile.py
+++ basic/excelFile.py
@@ -4,3 +4,19 @@ file = open('data.csv', 'w')
 file.write('김,이,박')
 file.write('\n한,송,노')
 file.close()
+
+
+
+
+
+# Ex.
+list = ['samsung' , 'kakao', 'naver', 'yahoo']
+
+file2 = open('list.txt', 'w')
+for i in list :
+  file2.write(i + '\n')
+
+file2.close()
+
+
+
