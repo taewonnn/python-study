938c38a253ce256b459456bd4ccb97580c699188
eden <eden@rainbow.co.kr>
2025-01-10 11:36:18 +0900

    list

--- basic/list.py
+++ basic/list.py
@@ -1,5 +1,6 @@
-# list
+# list - 자료형
 students = ["egoing", "sori", "maru"]
+
 
 print(students) # ['egoing', 'sori', 'maru']
 
@@ -18,6 +19,30 @@ print(min(grades)) # 2
 print(max(grades)) # 10
 
 print('-----')
+
+car = ['k5', 'white', ['5000', '6000']]
+print(car)
+print(car[0]) # k5
+print(car[1]) # white
+print(car[2]) # ['5000', '6000']
+
+car[1] = 'black'
+
+print(car) # ['k5', 'black', ['5000', '6000']]
+
+
+car2 = ['bmw 520','red', '100000']
+print(car2.pop()) # 100000
+
+
+
+# Dictionary - 객체
+car3 = {'brand': 'kia','model':'ddd', 'price':'10000' }
+
+print(car3['brand']) # kia
+
+
+
 
 
 # 통계 statistics
