203fdfe026c67d79fe4011ee2b3e0a38ba7973f1
eden <eden@rainbow.co.kr>
2025-01-10 11:50:37 +0900

    if

---
+++ basic/if.py
@@ -0,0 +1,24 @@
+# if 조건문
+
+stock = 10
+car = ['','']
+
+if(stock > 0) :
+  print('true!!')
+
+
+
+if( stock < 1) : 
+  print('fail!')
+else :
+  print('success')
+
+
+
+
+stock2 = ['k6', 'bmw', 'tico']
+
+if('k5' in stock2):
+  print('k5 available!!')
+elif( 'k6' in stock2) :
+  print('k6! avialable!!')
