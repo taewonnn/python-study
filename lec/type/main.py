50a7faa930acfdadb10216dceb8ef5d6f08735b3
taewonnn <thisis_tw94@naver.com>
2025-02-04 11:18:59 +0900

    type

---
+++ lec/type/main.py
@@ -0,0 +1,12 @@
+# type
+
+
+type_bool =     type(True)           # 참 또는 거짓
+type_integer =  type(10)             # 정수
+type_float =    type(3.14)           # 실수
+type_complex =  type(1 + 2j)         # 복소수
+type_string =   type("Hello")        # 문자열
+pass # 아무것도 하지 않는 코드. 여기에 break point 찍기
+
+
+
