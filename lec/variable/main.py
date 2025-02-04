e5a200a603ec1fa42cce0d6799f198d485eb8357
taewonnn <thisis_tw94@naver.com>
2025-02-04 11:08:00 +0900

    variable

--- lec/variable/main.py
+++ lec/variable/main.py
@@ -8,9 +8,36 @@ married = False
 print(last_name, first_name, age, married)
 
 
+# 한번에 지정
+x,y,z = True,2,3
+
+print(x,y,z)
+
+
 # f"{}"
 print(f"저는 {age}살입니다.")
 print(f"만으로는 {age - 1}살이죠.")
 print("자격 : " + ("있음" if age > 20 else "없음"))
 for candle in range(age):
     print("🕯️", end="")
+print("") # 줄바꿈
+
+
+
+print('!!')
+
+
+# input
+# 입력값 받기
+input_1 = input('첫 번째 값을 입력 \n')
+input_2 = input('두 번째 값을 입력 \n')
+input_3 = input('세 번째 값을 입력 \n')
+
+print(input_1, input_2, input_3)
+
+
+
+
+
+
+
