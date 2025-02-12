abf7285217f6cf53c22019728e459d6acd650485
taewonnn <thisis_tw94@naver.com>
2025-02-12 15:05:15 +0900

    module

---
+++ lec/module/greeting.py
@@ -0,0 +1,7 @@
+def hello_py(name):
+  return f"hello, {name}!"
+
+def bye_py(name):
+  return f"bye, {name}"
+
+version ="1.0"
---
+++ lec/module/main.py
@@ -0,0 +1,16 @@
+import greeting
+
+# 변수 사용
+greeting_version = greeting.version
+
+# method 사용
+hello_to_yalco = greeting.hello_py('yal')
+hello_to_yal = greeting.hello_py('yalco')
+
+print(hello_to_yalco)
+print(hello_to_yal)
+
+# hello, yal!
+# hello, yalco!
+
+pass
