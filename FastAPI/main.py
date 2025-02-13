35d4a5eb8748b080bbffa5da8bf2249763c18ca0
taewonnn <thisis_tw94@naver.com>
2025-02-13 18:34:04 +0900

    fast api

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -21,3 +21,15 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"items_id": item_id}
+  
+  
+  
+
+# http://127.0.0.1:8000/users
+# [
+# "Rick",
+# "Morty"
+# ]
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
