fa87545e03e22f1e1933bcfe887c7a60c99a8feb
taewonnn <thisis_tw94@naver.com>
2025-02-13 18:17:53 +0900

    fast api

---
+++ FastAPI/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# http://127.0.0.1:8000/
+
+# {
+# "message": "Hello World"
+# }
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+  
+  
+  
+# http://127.0.0.1:8000/items/poo
+# {
+# "items_id": "poo"
+# }
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"items_id": item_id}
