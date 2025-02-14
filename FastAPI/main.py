7e7d36d2c07231b0d3a2554f2fca6ce21acdb67c
taewonnn <thisis_tw94@naver.com>
2025-02-14 15:42:59 +0900

    fastapi

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-  
-  
+
+
 # http://127.0.0.1:8000/items/poo
 # {
 # "items_id": "poo"
@@ -20,15 +20,21 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"items_id": item_id}
-  
-  
-  
+
 
 # http://127.0.0.1:8000/users
 # [
-# "Rick",
+# "Rick"
+# ,
 # "Morty"
 # ]
 @app.get("/users")
 async def read_users():
     return ["Rick", "Morty"]
+
+
+
+fake_items_db = [{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"},{"item_name": "Baz"}]
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int =10):
+    return fake_items_db[skip: skip + limit]
